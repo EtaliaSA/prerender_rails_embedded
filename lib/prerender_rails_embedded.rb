0510b012049d69615ad9d59dea8b812df62886c3
Gian Carlo Pace <giancarlo.pace@gmail.com>
2015-01-29 06:33:04 +0100

    spawning a new thread for phantomjs that will be killed after 30 seconds if it doesn't end properly raised prerender resource timeout to 10s

--- lib/prerender_rails_embedded.rb
+++ lib/prerender_rails_embedded.rb
@@ -1,8 +1,51 @@
 module PrerenderRailsEmbedded
+  require 'timeout'
   require 'phantomjs'
 
   def self.flatten_js_to_html(url)
-    Phantomjs.run('--load-images=false', '--ignore-ssl-errors=true', '--ssl-protocol=tlsv1', '--disk-cache=yes', '--max-disk-cache-size=524228', "#{File.dirname(__FILE__)}/prerender_rails_embedded.js", url)
+    out = ''
+    thread = nil
+    pid = -1
+    begin
+    Timeout::timeout(30) do
+      thread = Thread.new do
+        IO.popen(phantomjs_invocation(url)) do |io|
+          pid = io.pid
+          Rails.logger.debug("Spawing new phantomjs processo with pid: #{pid}")
+          io.read
+        end
+      end
+      out += thread.value
+    end
+    rescue Timeout::Error
+      Rails.logger.debug('triggering timed out')
+
+      if pid_exists?(pid)
+        Rails.logger.warn("phantomjs with pid##{pid} timed out")
+
+        Process.kill 9, pid
+        out += '<html><head><meta name="robots" content="noindex, noarchive " /></head><body></body></html>'
+      end
+      if thread && thread.alive?
+        Thread.kill(thread)
+      end
+    end
+    out
+  end
+
+
+  def self.pid_exists?(pid)
+    begin
+      Process.kill 0, pid
+      return true
+    rescue Errno::ESRCH
+      return false
+    end
+
+  end
+
+  def self.phantomjs_invocation(url)
+    [Phantomjs.path , '--load-images=false', '--ignore-ssl-errors=true', '--ssl-protocol=TLSv1', '--disk-cache=yes', '--max-disk-cache-size=524228', "#{File.dirname(__FILE__)}/prerender_rails_embedded.js", url]
   end
 
   def self.local_renderer
